5a5ebbc0390d22fb86ba214580d16b787c6180ca
Pratham Raj <prathamrajgi@gmail.com>
2026-06-06 10:35:32 +0530

    feat: enhance getEmployeeById to include eligibility and compensation details

--- backend/src/controllers/employeeController.js
+++ backend/src/controllers/employeeController.js
@@ -1,30 +1,57 @@
 import { pool } from "../db.js"
+import {
+	calculateACP,
+	calculatePayout,
+	checkEligibility,
+} from "../utils/vrsCalculator.js"
 
 export const getEmployeeById = async (req, res) => {
 	try {
 		const { id } = req.params
-		const [rows] = await pool.query(
+
+		const [[employee]] = await pool.query(
 			"SELECT * FROM employees WHERE employee_id = ?",
 			[id],
 		)
-		const employee = rows[0]
 
-		if (!employee)
+		if (!employee) {
 			return res.status(404).json({ message: "Employee not found." })
-
-		let gradingHistory = []
-
-		if (employee.designation_type === "Executive") {
-			;[gradingHistory] = await pool.query(
-				`SELECT appraisal_year, grade, points
-				FROM grading_history
-				WHERE employee_id = ?
-				ORDER BY appraisal_year DESC`,
-				[id],
-			)
 		}
 
-		res.json({ profile: employee, gradingHistory })
+		const gradingHistory =
+			employee.designation_type === "Executive"
+				? (
+						await pool.query(
+							`SELECT appraisal_year, grade, points
+               FROM grading_history
+               WHERE employee_id = ?
+               ORDER BY appraisal_year DESC`,
+							[id],
+						)
+					)[0]
+				: []
+
+		const eligibility = checkEligibility(employee.dob, employee.doj)
+		const averageCreditPoint = calculateACP(gradingHistory)
+
+		const compensation = eligibility.isEligible
+			? calculatePayout(
+					employee.basic_pay,
+					employee.da,
+					eligibility.serviceYears,
+					eligibility.remainingYears,
+				)
+			: null
+
+		res.json({
+			profile: employee,
+			gradingHistory,
+			vrsDetails: {
+				eligibility,
+				averageCreditPoint,
+				compensation,
+			},
+		})
 	} catch (error) {
 		console.error(error)
 		res.status(500).json({ message: "Internal server error" })
